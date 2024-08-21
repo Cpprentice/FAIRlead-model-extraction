d4a88273e0f0dad3dbfeb0d1b5c2036acd7a94a9
Philipp Schmurr <philipp.schmurr@kit.edu>
2024-08-21 09:44:57 +0200

    moved 3_1 model to non-suffixed name now that the legacy model has been removed in the previous commit

--- generate-python-model.bat
+++ generate-python-model.bat
@@ -4,5 +4,5 @@ docker run --rm -v "%~dp0:/local" redocly/cli bundle /local/spec/0/openapi3_1.ya
 
 docker run --rm -v "%~dp0:/local" openapitools/openapi-generator-cli:latest generate ^
     -g python-fastapi ^
-    -o /local/simpler-model-3_1 ^
-    -c /local/generate-python-model-config-3_1.yaml
+    -o /local/simpler-model ^
+    -c /local/generate-python-model-config.yaml
