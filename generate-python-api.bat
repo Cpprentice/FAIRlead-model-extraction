bda9a045370ebccf93256f539b70d43bc631629f
Philipp Schmurr <philipp.schmurr@kit.edu>
2024-08-21 09:24:42 +0200

    removed legacy models after all plugins have been updated

--- generate-python-api.bat
+++ generate-python-api.bat
@@ -2,6 +2,5 @@
 
 docker run --rm -v "%~dp0:/local" openapitools/openapi-generator-cli:latest generate ^
     -g python-fastapi ^
-    --ignore-file-override /local/generate-python-api-ignore ^
     -o /local/simpler-api ^
     -c /local/generate-python-api-config.yaml
--- generate-python-model.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-docker run --rm -v "%~dp0:/local" openapitools/openapi-generator-cli:latest generate ^
-    -g python-fastapi ^
-    -o /local/simpler-model ^
-    -c /local/generate-python-model-config.yaml
