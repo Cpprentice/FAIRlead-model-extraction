2b043ffbca12672b71125135791621272be62c4c
Philipp Schmurr <philipp.schmurr@kit.edu>
2024-10-01 08:26:06 +0200

    updated api generation script to conform with 3.1 openapi version

--- generate-python-api.bat
+++ generate-python-api.bat
@@ -1,5 +1,7 @@
 @echo off
 
+docker run --rm -v "%~dp0:/local" redocly/cli bundle /local/spec/0/openapi3_1.yaml -o /local/spec/0/openapi3_1_merged.yaml
+
 docker run --rm -v "%~dp0:/local" openapitools/openapi-generator-cli:latest generate ^
     -g python-fastapi ^
     -o /local/simpler-api ^
